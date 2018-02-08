b5f051f8aa4b0f8981be70b75ae00f0eb188ae3e
ricco1337 <ricco1337@outlook.com>
2018-02-07 16:57:23 -0800

    static files2

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const path = require('path');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
+import HomePage from './components/home/homeComponents/home';
+import PortfolioPage from './components/portfolio/portfolio';
+import AboutPage from './components/about/about';
+import ResumePage from './components/resume/resume';
 
-const app = express()
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'))
-});
-let port = 80
-app.listen(port,() => console.log(`lisening on port ${port}`))
+ReactDOM.render(
+<Router>
+  <switch>
+  <Route exact path='/' component={HomePage} />
+    <Route path='/home' component={HomePage} />
+    <Route path='/portfolio' component={PortfolioPage}/>
+    <Route path='/about' component={AboutPage} />
+    <Route path='/resume' component={ResumePage}/>
+    </switch>
+    </Router>,
+document.getElementById('root'));
